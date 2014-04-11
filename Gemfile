3abf4df0f60a48c98e0b5f4362196235690b9419
Bartek Głowacki <bartosz.glowacki@kmpgroup.pl>
2014-04-11 11:05:46 +0200

    adding node modules and bowe modules

---
+++ Gemfile
@@ -0,0 +1,7 @@
+#ruby=2.1.1
+#ruby-gemset=chef_article
+
+source "https://rubygems.org"
+
+
+gem 'capistrano'
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,17 @@
 lock '3.1.0'
 
 set :application, 'deployapp'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:Vangerdahast/kmp_angular.git'
+set :user, "deploy"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -27,9 +28,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{node_modules app/bower_components}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :ssh_options, { :forward_agent => true }
+set :deploy_via, :remote_cache
+set :rbenv_ruby, '2.1.1'
+set :rbenv_type, :user
 
 set :default_env, {
     path: ["/usr/local/rbenv/shims",
@@ -46,7 +48,9 @@ namespace :setup do
   task :yo do
     on roles(:app), in: :sequence, wait: 5 do
       within release_path do
-        execute :npm, "install yo"
+        execute :sudo, "npm install -g yo"
+        execute :sudo, "npm install -g grunt-cli"
+        execute :sudo, "npm install -g bower"
       end
     end
   end
